e63ce354f143b77e8f4473728c410470a2227480
moscosky <eren_cizme@hotmail.com>
2015-06-09 18:08:48 +0300

    updated

--- cgi-bin/getcmd.py
+++ cgi-bin/getcmd.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python
 import cgitb; cgitb.enable()
-
-# The subprocess module is new in 2.4
 import os, urllib, subprocess as sub
+from coff import *
 
 # Retrieve the command from the query string
 # and unencode the escaped %xx chars
 str_command = urllib.unquote(os.environ['QUERY_STRING'])
+coeff = []
 
-p = sub.Popen(['/bin/bash', '-c', str_command], 
-    stdout=sub.PIPE, stderr=sub.STDOUT)
-output = urllib.unquote(p.stdout.read())
+if str_command.split("=")[0] == "low":
+    b, a = lowpass(str_command.split("=")[1])
+    coeff.append("00" + convert(b[0]))
+    coeff.append("01" + convert(b[1]))
+    coeff.append("02" + convert(b[2]))
+    coeff.append("03" + convert(a[1]))
+    coeff.append("04" + convert(a[2]))
+    
+#elif str_command.split("=")[0] == "band":
+#    a = lowpass(str_command.split("=")[1])
+#    strcmd = "echo 01AABBCCDD > /proc/superip"
+
+elif str_command.split("=")[0] == "high":
+    b, a = highpass(str_command.split("=")[1])
+    coeff.append("10" + convert(b[0]))
+    coeff.append("11" + convert(b[1]))
+    coeff.append("12" + convert(b[2]))
+    coeff.append("13" + convert(a[1]))
+    coeff.append("14" + convert(a[2]))
+
+
+for x in coeff:
+    strcmd = "echo " + x + " > /proc/superip"
+    p = sub.Popen(['/bin/bash', '-c', strcmd], 
+        stdout=sub.PIPE, stderr=sub.STDOUT)
+    output = urllib.unquote(p.stdout.read())
+
+
+
 
 print """\
 Content-Type: text/html\n
@@ -20,4 +46,4 @@ $ %s
 %s
 </pre>
 </body></html>
-""" % (str_command, output)
+""" % (strcmd, output)
